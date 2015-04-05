fa66139e1125af632156aa81c3d0fe7cb7f190a4
aleksandr <aatarasoff@gmail.com>
2015-04-05 23:36:04 +0300

    indent

--- elastic/elastic.go
+++ elastic/elastic.go
@@ -1,12 +1,12 @@
 package elastic
 
 import (
-	"net/url"
-	"errors"
+  "net/url"
+  "errors"
   "encoding/json"
   "time"
 
-	"github.com/gliderlabs/registrator/bridge"
+  "github.com/gliderlabs/registrator/bridge"
   elasticapi "github.com/olivere/elastic"
 )
 
@@ -18,7 +18,7 @@ type Container struct {
 }
 
 func init() {
-	bridge.Register(new(Factory), "elastic")
+  bridge.Register(new(Factory), "elastic")
 }
 
 type Factory struct{}
@@ -27,35 +27,35 @@ func (f *Factory) New(uri *url.URL) bridge.RegistryAdapter {
   urls := "http://127.0.0.1:9200"
 
   if uri.Host != "" {
-		urls = "http://"+uri.Host
-	}
+    urls = "http://"+uri.Host
+  }
 
   client, err := elasticapi.NewClient(elasticapi.SetURL(urls))
   if err != nil {
     log.Fatal("elastic: ", uri.Scheme)
   }
 
-	return &ElasticAdapter{client: client}
+  return &ElasticAdapter{client: client}
 }
 
 type ElasticAdapter struct {
-	client   *elasticapi.Client
+  client   *elasticapi.Client
 }
 
 func (r *ElasticAdapter) Ping() error {
-	status := r.client.IsRunning()
+  status := r.client.IsRunning()
 
   if !status {
     return errors.New("client is not Running")
   }
 
-	return nil
+  return nil
 }
 
 func (r *ElasticAdapter) Register(service *bridge.Service) error {
   serviceAsJson, err := json.Marshal(service)
   if err != nil {
-      return err
+    return err
   }
 
   timestamp := time.Now().Local().Format("2006-01-02T15:04:05.000Z07:00")
@@ -63,21 +63,21 @@ func (r *ElasticAdapter) Register(service *bridge.Service) error {
   // Add a document to the index
   container := Container{Name: service.Name, Action: "start", Message: string(serviceAsJson), Timestamp: timestamp}
   _, err = r.client.Index().
-      Index("containers").
-      Type("audit").
-      BodyJson(container).
-      Timestamp(timestamp).
-      Do()
+    Index("containers").
+    Type("audit").
+    BodyJson(container).
+    Timestamp(timestamp).
+    Do()
   if err != nil {
-      return err
+    return err
   }
-	return nil
+  return nil
 }
 
 func (r *ElasticAdapter) Deregister(service *bridge.Service) error {
   serviceAsJson, err := json.Marshal(service)
   if err != nil {
-      return err
+    return err
   }
 
   timestamp := time.Now().Local().Format("2006-01-02T15:04:05.000Z07:00")
@@ -85,17 +85,17 @@ func (r *ElasticAdapter) Deregister(service *bridge.Service) error {
   // Add a document to the index
   container := Container{Name: service.Name, Action: "stop", Message: string(serviceAsJson), Timestamp: timestamp}
   _, err = r.client.Index().
-      Index("containers").
-      Type("audit").
-      BodyJson(container).
-      Timestamp(timestamp).
-      Do()
+    Index("containers").
+    Type("audit").
+    BodyJson(container).
+    Timestamp(timestamp).
+    Do()
   if err != nil {
-      return err
+    return err
   }
-	return nil
+  return nil
 }
 
 func (r *ElasticAdapter) Refresh(service *bridge.Service) error {
-	return nil
+  return nil
 }
